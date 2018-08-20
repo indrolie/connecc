cac999d631dfd7cf1c827ae7c9772ecd98bfe502
Indro Lie <Indrolie@gmail.com>
2018-08-20 22:26:53 +0700

    add heading

--- reactjs/src/Heading1/index.js
+++ reactjs/src/Heading1/index.js
@@ -1,1 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react';
+
+class Heading1 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      text: 'Sign up'
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  render() {
+    return <p>{this.props.text}</p>;
+  }
+}
+
+export default Heading1;
