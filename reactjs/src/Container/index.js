cda4c8c05043c4fa9beb7558842d9a279004ca16
Indro Lie <Indrolie@gmail.com>
2018-08-20 22:27:08 +0700

    modify container class

--- reactjs/src/Container/index.js
+++ reactjs/src/Container/index.js
@@ -1,5 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const Container = ({ children }) => <div className="container">{children}</div>
+const Container = ({ children }) => (
+  <div className="container">
+    <div className="card animated fadeIn">
+      <div className="card-body">
+        <div id="card-content-wrapper">{children}</div>
+      </div>
+    </div>
+  </div>
+);
 
-export default Container
+export default Container;
